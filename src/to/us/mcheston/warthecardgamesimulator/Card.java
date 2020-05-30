1c6986584f7d25cf89cd26e6169913b49e96a81f
Mike Heston <m45t3rhest@gmail.com>
2020-05-30 15:42:15 -0700

    New: 	Added two lists for the different ranks and suits to be checked against. 	Added one map to store the declared values. ("rank" & "suit") 	Added card constructor to make sure that when a new card object is created it will have two correct values. 	Added getInfo() to return the map value.

--- src/to/us/mcheston/warthecardgamesimulator/Card.java
+++ src/to/us/mcheston/warthecardgamesimulator/Card.java
@@ -10,38 +10,24 @@ import java.util.Map;
  * stores card rank and suit and handle the setting and getting of info.
  */
 public class Card {
-	private List<Character> posValue = Arrays.asList('A','2','3','4','5','6','7','8','9','0','J','Q','K');
+	private List<Character> posRank = Arrays.asList('A','2','3','4','5','6','7','8','9','0','J','Q','K');
 	private List<Character> posSuit = Arrays.asList('S','H','D','C'); //(S)pades,(H)earts,(D)iamonds,(C)lubs
 	
 	private Map<String, Character> info = new HashMap<String, Character>(); //official value and suit of card
 	
-	public Card(char v, char s) {
+	//sets the values for the card.
+	public Card(char r, char s) {
 		//Check if passed parameter is valid
-		if(!posValue.contains(v)) {
-			throw new IllegalArgumentException(v + " :is not a valid card value"); //throws an exception if check failed
+		if(!posRank.contains(r)) {
+			throw new IllegalArgumentException(r + " :is not a valid card rank"); //throws an exception if check failed
 		}
 		//Check if passed parameter is valid
 		if(!posSuit.contains(s)) {
 			throw new IllegalArgumentException(s + " :is not a valid card suit"); //throws an exception if check failed
 		}
 		
-		info.put("value", v); //assign v to info with key "value"
+		info.put("rank", r); //assign v to info with key "rank"
 		info.put("suit", s); //assign s to info with key "suit"
-	}
-	
-	public void setInfo(char v, char s) {
-		//Check if passed parameter is valid
-		if(!posValue.contains(v)) {
-			throw new IllegalArgumentException(v + " :is not a valid card value"); //throws an exception if check failed
-		}
-		//Check if passed parameter is valid
-		if(!posSuit.contains(s)) {
-			throw new IllegalArgumentException(s + " :is not a valid card suit"); //throws an exception if check failed
-		}
-		
-		info.put("value", v); //assign v to info with key "value"
-		info.put("suit", s); //assign s to info with key "suit"
-		
 	}
 	
 	public Map<String, Character> getInfo(){
